948c850ac05c83db17e35c61599e945a43cc1f6c
rayl <dwadeflashback@gmail.com>
2025-06-22 00:03:25 -0400

    tmp

--- testDeployGCP/Controllers/HomeController.cs
+++ testDeployGCP/Controllers/HomeController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
 using System.Diagnostics;
 using testDeployGCP.Models;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
 
 namespace testDeployGCP.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly string _connectionString = "";
+
+        [HttpPost]
+        public async Task<IActionResult> InsertCloudSql()
+        {
+            try
+            {
+                using (var conn = new SqlConnection(_connectionString))
+                using (var cmd = new SqlCommand("InsertGcpSql", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    await conn.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+
+                return Json(new { success = true, message = "Stored procedure executed." });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+        }
 
         public HomeController(ILogger<HomeController> logger)
         {
